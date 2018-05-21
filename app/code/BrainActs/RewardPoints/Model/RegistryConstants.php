548b2ab6ec13bdb9b6a78d0233db46d16a173078
upettools <marketing@upettools.com>
2018-05-21 15:06:35 +0800

    add points

---
+++ app/code/BrainActs/RewardPoints/Model/RegistryConstants.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) 2017 BrainActs Commerce OÜ, All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace BrainActs\RewardPoints\Model;
+
+/**
+ * Class RegistryConstants
+ *
+ * @author BrainActs Core Team <support@brainacts.com>
+ */
+class RegistryConstants
+{
+    /**
+     * Key for current rule in registry
+     */
+    const CURRENT_REWARD_POINTS_RULE = 'current_reward_points_earning_rule';
+}
