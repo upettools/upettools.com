d24dcf765a735cc6c598ccd639a8e8c9ee1e200d
upettools <marketing@upettools.com>
2018-06-12 16:32:25 +0800

    .edit

---
+++ app/code/Become/Wholesale/Block/Form.php
@@ -0,0 +1,10 @@
+<?php
+namespace Become\Wholesale\Block;
+ 
+class Form extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
